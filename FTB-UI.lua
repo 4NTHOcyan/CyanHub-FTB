5348b085bb8fe1549f8e0386b5eb10eaf2841ae0
4NTHO <zandemon.business@gmail.com>
2026-02-24 04:52:13 -0600

    Update FTB-UI.lua
    Added new teleports and no-clipping
    
    Fixed code not running after death

--- FTB-UI.lua
+++ FTB-UI.lua
@@ -2,7 +2,7 @@
 
 if game.PlaceId == 109686116036889 then
 
-    local _Version = "CyanHub v1.0"
+    local _Version = "CyanHub v1.1-beta"
 
     --// call Library
     local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
@@ -34,6 +34,7 @@ if game.PlaceId == 109686116036889 then
 
     local _Invincible_ = false -- Invincible toggle flag
     local _Pickup_ = false -- Food pick up flag
+    local _naurclip = false -- noclip flag
 
     local _HurtyParts = workspace.TGaz -- toxic gas in rare wh
 
@@ -58,43 +59,66 @@ if game.PlaceId == 109686116036889 then
 	    Max = 130,
 	    Callback = function(value) 
         
-            __HUM_player.WalkSpeed = value
+            local charr_ = game.Players.LocalPlayer.Character
+            local __hum_PLAYER = charr_.Humanoid
+
+            __hum_PLAYER.WalkSpeed = value
+
+            if not charr_ or charr_:FindFirstChildOfClass("Humanoid").Health == 0 then
+                local plr_wait = game.Players.LocalPlayer.CharacterAdded:Wait()
+                plr_wait:FindFirstChild("Humanoid").WalkSpeed = value
+            end
         
         end
 
     }
 
-    PLAYERTAB:Button{
-        Name = "Go To Home",
-	    Description = "Takes you back to your bunker",
-	    Callback = function() 
-        
-            local __TP_NC_x = _homex__.CFrame
-
-            _me_inWS.CFrame = __TP_NC_x * CFrame.new(0, 2, 0)
-        
-        end
-
-    }
-
-    PLAYERTAB:Button{
-        Name = "Go To Night Crystal",
-	    Description = "Teleports you to the underground Night Crystal",
-	    Callback = function() 
-        
-            local __TP_NC_x = NC__model.CFrame
-
-            _me_inWS.CFrame = __TP_NC_x * CFrame.new(0, 2, 0)
-        
-        end
-
-    }
-
-    -- add button for rare furniture and Street for food drop
 
     PLAYERTAB:Toggle{
 
-        Name = "No Die (Simple)",
+        Name = "No Clip",
+	    StartingState = false,
+	    Description = "Go through stuff",
+	    Callback = function(state) 
+        
+           _naurclip = state
+            while _naurclip do
+
+                local __plr = game.Players.LocalPlayer.Character
+
+                for _,v in pairs(__plr:GetChildren()) do
+                    task.wait()
+                    if v:IsA("BasePart") and v.CanCollide == true then
+                        task.wait()
+                        v.CanCollide = false
+                        v.CanQuery = false
+                        v.CanTouch = false
+                        task.wait()
+                    end
+                end
+
+                if _naurclip == false then
+                    for _,v in pairs(__plr:GetChildren()) do
+                        task.wait()
+                        if v:IsA("BasePart") and v.CanCollide == false then  --// left off here with changes
+                            task.wait()
+                            v.CanCollide = true
+                            v.CanQuery = true
+                            v.CanTouch = true
+                            task.wait()
+                        end
+                    end
+                end
+
+            end
+        
+        end
+    }
+
+
+    PLAYERTAB:Toggle{
+
+        Name = "No Die (experimental*)",
 	    StartingState = false,
 	    Description = "Do not die to Hunger or Toxic Gas",
 	    Callback = function(state) 
@@ -126,10 +150,11 @@ if game.PlaceId == 109686116036889 then
                 _HurtyParts.CanTouch = false
 
                 pcall(___DISABLe)
-                while true do
-                    pcall(___RESETmaX)
-                    task.wait()
-                end
+                task.wait()
+                
+                pcall(___RESETmaX)
+                task.wait()          -- !!! changed -- removed while loop
+                
 
             end
         
@@ -154,8 +179,6 @@ if game.PlaceId == 109686116036889 then
                         local __HandleF = v:FindFirstChild("Handle")
                     
                         if __HandleF then
-                        
-                            task.wait()
 
                             local __PROX = __HandleF:FindFirstChild("ProximityPrompt")
                         
@@ -175,8 +198,6 @@ if game.PlaceId == 109686116036889 then
                         local __HandleF = v:FindFirstChild("Handle")
                     
                         if __HandleF then
-                        
-                            task.wait()
 
                             local __PROX = __HandleF:FindFirstChild("ProximityPrompt")
                         
@@ -196,8 +217,6 @@ if game.PlaceId == 109686116036889 then
                         local __HandleF = v:FindFirstChild("Handle")
                     
                         if __HandleF then
-                        
-                            task.wait()
 
                             local __PROX = __HandleF:FindFirstChild("ProximityPrompt")
                         
@@ -295,5 +314,177 @@ if game.PlaceId == 109686116036889 then
 
 
     }
+
+    ----------
+    --// Teleport Tab
+
+    local TP = GUI:Tab{
+        Name = "Teleport",
+        Icon = nil,
+    }
+
+    TP:Button{
+        Name = "Go To Home",
+	    Description = "Teleports you to your bunker",
+	    Callback = function() 
+        
+            local __TP_NC_x = _homex__.CFrame
+
+
+            local charr_ = game.Players.LocalPlayer.Character
+            local __hum_PLAYER = charr_:FindFirstChild("HumanoidRootPart")
+            __hum_PLAYER.CFrame = __TP_NC_x * CFrame.new(0, 2, 0)
+
+            if not __hum_PLAYER or charr_:FindFirstChildOfClass("Humanoid").Health == 0 then
+                local plr_wait = game.Players.LocalPlayer.CharacterAdded:Wait()
+                plr_wait.Humanoid:FindFirstChild("HumanoidRootPart").CFrame = __TP_NC_x * CFrame.new(0, 2, 0)
+            end
+     
+        end
+
+    }
+
+    TP:Button{
+        Name = "Go To Night Crystal",
+	    Description = "Teleports you to the underground Night Crystal",
+	    Callback = function() 
+
+            local __TP_NC_x = NC__model.CFrame
+
+            local charr_ = game.Players.LocalPlayer.Character
+            local __hum_PLAYER = charr_:FindFirstChild("HumanoidRootPart")
+            __hum_PLAYER.CFrame = __TP_NC_x * CFrame.new(0, 2, 0)
+
+            if not __hum_PLAYER or charr_:FindFirstChildOfClass("Humanoid").Health == 0 then
+                local plr_wait = game.Players.LocalPlayer.CharacterAdded:Wait()
+                plr_wait.Humanoid:FindFirstChild("HumanoidRootPart").CFrame = __TP_NC_x * CFrame.new(0, 2, 0)
+            end
+        
+        end
+
+    }
+
+    TP:Button{
+        Name = "Go To Rare Furniture",
+	    Description = "Teleports you to the Rare Furniture inside of the Market",
+	    Callback = function() 
+
+            local charr_ = game.Players.LocalPlayer.Character
+            local __hum_PLAYER = charr_:FindFirstChild("HumanoidRootPart")
+            __hum_PLAYER.CFrame = CFrame.new(-190, 5, 35)
+
+            if not __hum_PLAYER or charr_:FindFirstChildOfClass("Humanoid").Health == 0 then
+                local plr_wait = game.Players.LocalPlayer.CharacterAdded:Wait()
+                plr_wait.Humanoid:FindFirstChild("HumanoidRootPart").CFrame = CFrame.new(-190, 5, 35)
+            end
+        
+        end
+
+    }
+
+    TP:Button{
+        Name = "Go To Street",
+	    Description = "Teleports you to the main road",
+	    Callback = function() 
+
+            local charr_ = game.Players.LocalPlayer.Character
+            local __hum_PLAYER = charr_:FindFirstChild("HumanoidRootPart")
+            __hum_PLAYER.CFrame = CFrame.new(290, 5, 35)
+
+            if not __hum_PLAYER or charr_:FindFirstChildOfClass("Humanoid").Health == 0 then
+                local plr_wait = game.Players.LocalPlayer.CharacterAdded:Wait()
+                plr_wait.Humanoid:FindFirstChild("HumanoidRootPart").CFrame = CFrame.new(290, 5, 35)
+            end
+        
+        end
+
+    }
+
+    TP:Button{
+        Name = "Go To Market",
+	    Description = "Teleports you to the regular market entrance",
+	    Callback = function() 
+
+            local charr_ = game.Players.LocalPlayer.Character
+            local __hum_PLAYER = charr_:FindFirstChild("HumanoidRootPart")
+            __hum_PLAYER.CFrame = CFrame.new(100, 5, -125)
+
+            if not __hum_PLAYER or charr_:FindFirstChildOfClass("Humanoid").Health == 0 then
+                local plr_wait = game.Players.LocalPlayer.CharacterAdded:Wait()
+                plr_wait.Humanoid:FindFirstChild("HumanoidRootPart").CFrame = CFrame.new(100, 5, -125)
+            end
+        
+        end
+
+    }
+
+        -- add button for Street for food drop
+
+
+    ----------
+    --// Furniture Tab -- TEST Suite !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    local _F = GUI:Tab{
+        Name = "Furniture (beta in progress)",
+        Icon = nil,
+    }
+
+
+    local selected = nil
+
+
+    local function ReturnFurniture()
+        local Names = {}
+    
+        for _, item in pairs(workspace.Wyposazenie:GetChildren()) do
+            if item:IsA("Folder") then
+                for _, interno in pairs(item:GetChildren()) do
+                    if interno:IsA("Model") and not table.find(Names, interno.Name) then
+                        table.insert(Names, interno.Name)
+                end
+            end
+
+            elseif item:IsA("Model") and not table.find(Names, item.Name) then
+                table.insert(Names, item.Name)
+            end
+
+        end
+    
+        return Names
+    end
+
+
+    local function GetFurniture()
+        for _, furniture in pairs(workspace.Wyposazenie:GetChildren()) do
+            if furniture:IsA("Folder") then
+                for _, interno in pairs(furniture:GetChildren()) do
+                    if interno:IsA("Model") and interno.Name == selected then
+                        game:GetService("ReplicatedStorage").PickupItemEvent:FireServer(interno)
+                        return true
+                    end
+            end
+
+            elseif furniture:IsA("Model") and furniture.Name == selected then
+                game:GetService("ReplicatedStorage").PickupItemEvent:FireServer(furniture)
+                return true
+            end
+        end
+    
+        return false
+    end
+
+
+
+    local furnitureDrop = _F:Dropdown{
+
+        Name = "Furniture List",
+    	StartingText = "Select...",
+    	Description = nil,
+        Items = Names,
+        Callback = function(item) return end
+
+    }
+
+
 
 end
