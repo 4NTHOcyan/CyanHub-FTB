59b3e14888de26b1f3bd04abc4931bc372cc19da
4NTHO <zandemon.business@gmail.com>
2026-02-25 18:34:10 -0600

    secure FTB-UI.lua
    added key sec

--- FTB-UI.lua
+++ FTB-UI.lua
@@ -1,4 +1,4 @@
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/4NTHOcyan/CyanHub-FTB/refs/heads/main/ftb-jk"))() -- holder )
 
 if game.PlaceId == 109686116036889 then
 
@@ -629,3 +629,4 @@ if game.PlaceId == 109686116036889 then
     }
 
 end
+
